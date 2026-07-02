6f9d2bc41323efacb6806868692906c393a31c12
Uttam demo <chaurasiauttam33@gmail.com>
2026-07-02 22:57:06 +0530

    feat(orders): register Trade model and MatchingEngine in OrdersModule

--- papertrade-backend/src/orders/orders.module.ts
+++ papertrade-backend/src/orders/orders.module.ts
@@ -4,6 +4,8 @@ import { OrderController } from './orders.controller'
 import { OrdersService } from './orders.service'
 import { Order, OrderSchema } from './orders.schema'
 import { User, UserSchema } from '../users/users.schema'
+import { Trade, TradeSchema } from '../trades/trade.schema'
+import { MatchingEngine } from './matching.engine'
 import { StocksModule } from '../stocks/stocks.module'
 import { CacheModule } from '../cache/cache.module'
 
@@ -12,12 +14,13 @@ import { CacheModule } from '../cache/cache.module'
         MongooseModule.forFeature([
             {name: Order.name, schema: OrderSchema },
             {name: User.name, schema: UserSchema },
+            {name : Trade.name, schema: TradeSchema },
         ]),
         StocksModule,
         CacheModule,
     ],
     controllers: [OrderController],
-    providers: [OrdersService],
-    exports: [OrdersService],
+    providers: [OrdersService, MatchingEngine],
+    exports: [OrdersService, MatchingEngine],
 })
 export class OrdersModule {}
